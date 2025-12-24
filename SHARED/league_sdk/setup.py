d2a4cf0109bb7a529332a9a89c26b51bafbc3995
sheldon <talshel@wix.com>
2025-12-24 15:00:00 +0200

    Package SDK for installation
    - Add setup.py for editable package installation
    - Define SDK dependencies in requirements.txt

---
+++ SHARED/league_sdk/setup.py
@@ -0,0 +1,11 @@
+"""Setup for league_sdk."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="league_sdk",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
+
